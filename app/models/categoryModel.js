cbea76155297ecea81f9fa1e8cbeb27cd173e05f
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2024-03-23 09:21:52 -0400

    Define Category Schema

--- app/models/categoryModel.js
+++ app/models/categoryModel.js
@@ -1,1 +1,28 @@
-/* WRITE YOUR CODE HERE */
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    createdTs: {
+        type: Date,
+        default: new Date(),
+    },
+    updatedTs: {
+        type: Date,
+        default: new Date(),
+    }
+});
+
+const CategoryModel = mongoose.model('category', categorySchema);
+
+module.exports = CategoryModel;
