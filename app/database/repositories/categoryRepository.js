6d8bcb7411705a67038ed7b75b9bba49f26e6d0b
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2024-03-23 09:27:42 -0400

    CRUD Operations Pt I: Category

--- app/database/repositories/categoryRepository.js
+++ app/database/repositories/categoryRepository.js
@@ -1,23 +1,73 @@
 const CategoryModel = require("../../models/categoryModel");
 
 const createCategory = async (name, description) => {
-  /* WRITE YOUR CODE HERE */
+  try {
+    const newCategory = await CategoryModel.create({
+      name: name,
+      description: description,
+    });
+    return newCategory;
+  } catch (err) {
+    throw new Error(`Error while creating category: ${err.message}`);
+  }
 };
 
 const editCategory = async (categoryId, newData) => {
-  /* WRITE YOUR CODE HERE */
+  try {
+    const categoryObject = await CategoryModel.findOne({
+      _id: categoryId,
+      isActive: true,
+    });
+
+    if (!categoryObject) {
+      return null;
+    }
+
+    categoryObject.name = newData.name;
+    categoryObject.description = newData.description;
+
+    const updatedCategory = await categoryObject.save();
+    return updatedCategory;
+  } catch (err) {
+    throw new Error(`Error while editing category: ${err.message}`);
+  }
 };
 
 const deleteCategory = async (categoryId) => {
-  /* WRITE YOUR CODE HERE */
+  try {
+    const categoryObject = await CategoryModel.findById(categoryId);
+
+    if (!categoryObject) {
+      return null;
+    }
+
+    categoryObject.isActive = false;
+    const updatedCategory = await categoryObject.save();
+    return updatedCategory;
+  } catch (err) {
+    throw new Error(`Error while deleting category: ${err.message}`);
+  }
 };
 
 const getCategory = async (categoryId) => {
-  /* WRITE YOUR CODE HERE */
+  try {
+    const categoryObject = await CategoryModel.findOne({
+      _id: categoryId,
+      isActive: true,
+    });
+    return categoryObject;
+  } catch (err) {
+    throw new Error(`Error while fetching category: ${err.message}`);
+  }
 };
 
 const getAllCategories = async () => {
-  /* WRITE YOUR CODE HERE */
+  try {
+    const categories = await CategoryModel.find({ isActive: true });
+    return categories;
+  } catch (err) {
+    throw new Error(`Error while fetching categories: ${err.message}`);
+  }
 };
 
 module.exports = {
